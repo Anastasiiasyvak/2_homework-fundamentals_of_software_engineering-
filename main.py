c410c34099fa7212586e6f3ee0794291c7a6976d
Anastasiiasyvak <asyvak@kse.org.ua>
2023-09-30 18:29:41 +0300

    Fixes #6

---
+++ main.py
@@ -0,0 +1,49 @@
+import requests # доступ до нашого API
+from datetime import datetime, timedelta
+
+def last_seen_task(last_seen):
+    now = datetime.now()  # поточний час
+    last_seen = last_seen.split(".")[0]
+    last_time_online = datetime.fromisoformat(last_seen)
+    time = now - last_time_online
+
+    if time < timedelta(seconds=30):
+        return "just now"
+    elif time < timedelta(minutes=1):
+        return "less than a minute ago"
+    elif time < timedelta(minutes=60):
+        return "couple of minutes ago"
+    elif time < timedelta(minutes=120):
+        return "hour ago"
+    elif time < timedelta(hours=24):
+        return "today"
+    elif time < timedelta(hours=48):
+        return "yesterday"
+    elif time < timedelta(days=7):
+        return "this week"
+    else:
+        return "long time ago"
+
+offset = 0
+
+url = f'https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    users = data.get('data', [])
+
+    if users:
+        for user in users:
+            username = user.get('nickname', 'unknown user')
+            last_seen = user.get('lastSeenDate', None)
+            if last_seen:
+                time_of_visit = format_last_seen(last_seen)
+                print(f"{username} was online {time_of_visit}")
+            else:
+                print(f"{username} is online now")
+    else:
+        print("User data not found in the API response")
+else:
+    print(f"Unable to retrieve data. Status code: {response.status_code}")
