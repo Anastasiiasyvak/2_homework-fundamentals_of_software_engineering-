c4bd59daa0a3ccb630b9f773fba0935b752e5262
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-01 16:17:35 +0300

    add languages (localization)

--- main.py
+++ main.py
@@ -2,11 +2,53 @@ import requests
 from datetime import datetime, timedelta
 import pytz
 
-
 timezone = pytz.timezone("Europe/Kyiv")
 
+languagees = {
+    'eng': {
+        'just_now': 'just now',
+        'less_than_a_minute_ago': 'less than a minute ago',
+        'couple_of_minutes_ago': 'couple of minutes ago',
+        'hour_ago': 'hour ago',
+        'today': 'today',
+        'yesterday': 'yesterday',
+        'this_week': 'this week',
+        'long_time_ago': 'long time ago'
+    },
+    'ukr': {
+        'just_now': 'щойно',
+        'less_than_a_minute_ago': 'менше хвилини тому',
+        'couple_of_minutes_ago': 'декілька хвилин тому',
+        'hour_ago': 'годину тому',
+        'today': 'сьогодні',
+        'yesterday': 'вчора',
+        'this_week': 'цього тижня',
+        'long_time_ago': 'давно'
+    },
 
-def last_seen_task(last_seen):
+        'it': {
+        'just_now': 'proprio ora',
+        'less_than_a_minute_ago': 'meno di un minuto fa',
+        'couple_of_minutes_ago': 'alcuni minuti fa',
+        'hour_ago': "un'ora fa",
+        'today': 'oggi',
+        'yesterday': 'ieri',
+        'this_week': 'questa settimana',
+        'long_time_ago': 'molto tempo fa'
+    },
+    'fr': {
+        'just_now': 'juste maintenant',
+        'less_than_a_minute_ago': 'il y a moins d une minute',
+        'couple_of_minutes_ago': 'il y a quelques minutes',
+        'hour_ago': 'il y a une heure',
+        'today': 'aujourd hui',
+        'yesterday': 'hier',
+        'this_week': 'cette semaine',
+        'long_time_ago': 'il y a longtemps'
+    }
+}
+
+def last_seen_task(last_seen, selected_language):
     now = datetime.now(timezone)
     last_seen = last_seen.split(".")[0]
     last_time_online = datetime.fromisoformat(last_seen)
@@ -15,21 +57,21 @@ def last_seen_task(last_seen):
     time = now - last_time_online
 
     if time < timedelta(seconds=30):
-        return "just now"
+        return languagees[selected_language]['just_now']
     elif time < timedelta(minutes=1):
-        return "less than a minute ago"
+        return languagees[selected_language]['less_than_a_minute_ago']
     elif time < timedelta(minutes=60):
-        return "couple of minutes ago"
+        return languagees[selected_language]['couple_of_minutes_ago']
     elif time < timedelta(minutes=120):
-        return "hour ago"
+        return languagees[selected_language]['hour_ago']
     elif time < timedelta(hours=24):
-        return "today"
+        return languagees[selected_language]['today']
     elif time < timedelta(hours=48):
-        return "yesterday"
+        return languagees[selected_language]['yesterday']
     elif time < timedelta(days=7):
-        return "this week"
+        return languagees[selected_language]['this_week']
     else:
-        return "long time ago"
+        return languagees[selected_language]['long_time_ago']
 
 
 def fetch_user_data(offset):
@@ -60,13 +102,15 @@ def get_user_data():
 
 
 def main():
+    selected_language = input("Choose language (ukr, eng, it, fr): ")
+
     user_data = get_user_data()
 
     for user in user_data:
         username = user.get('nickname', 'unknown user')
         last_seen = user.get('lastSeenDate', None)
         if last_seen:
-            time_of_visit = last_seen_task(last_seen)
+            time_of_visit = last_seen_task(last_seen, selected_language)
             print(f"{username} was online {time_of_visit}")
         else:
             print(f"{username} now online")
