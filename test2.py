b0191a15e04671dd4ffbb44381879889cd3444a3
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-14 19:41:04 +0300

    verified that data is correct in integration tests and removed feature numbers form tests too

--- test2.py
+++ test2.py
@@ -5,38 +5,61 @@ from feature1 import app
 client = TestClient(app)
 
 
-def test_feature1_integration():
+def test_historical_data_for_all_user_integration():
     date = datetime(2023, 9, 27, 20, 0)
     response = client.get(f"/api/stats/users?date={date}")
 
     assert response.status_code == 200
     data = response.json()
+
     assert "usersOnline" in data
+    users_online = data["usersOnline"]
+
+    assert isinstance(users_online, int)
+    assert users_online >= 0
 
 
-def test_feature2_integration():
+def test_historical_data_for_concrete_user_integration():
     date = datetime(2023, 9, 27, 20, 0)
     user_id = "A4DC2287-B03D-430C-92E8-02216D828709"
     response = client.get(f"/api/stats/user?date={date}&userId={user_id}")
 
     assert response.status_code == 200
     data = response.json()
+
     assert "wasUserOnline" in data
     assert "nearestOnlineTime" in data
 
+    was_user_online = data["wasUserOnline"]
 
-def test_feature3_integration():
+    assert isinstance(was_user_online, bool)
+
+
+def test_prediction_mechanism_on_historical_data_for_user_count_integration():
     date = datetime(2025, 9, 27, 20, 0)
     response = client.get(f"/api/predictions/users?date={date}")
 
     assert response.status_code == 200
     data = response.json()
+
     assert "onlineUsers" in data
+    online_users = data["onlineUsers"]
+
+    assert isinstance(online_users, int)
+    assert online_users >= 0
 
 
-def test_feature4_integration():
+def test_preduction_mechanism_on_historical_data_concrete_user_integration():
     date = datetime(2025, 9, 27, 20, 0)
     user_id = "A4DC2287-B03D-430C-92E8-02216D828709"
     response = client.get(f"/api/predictions/user?date={date}&userId={user_id}&tolerance=0.85")
 
-    assert response.status_code
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "willBeOnline" in data
+    assert "onlineChance" in data
+
+    will_be_online = data["willBeOnline"]
+
+    assert isinstance(will_be_online, bool)
