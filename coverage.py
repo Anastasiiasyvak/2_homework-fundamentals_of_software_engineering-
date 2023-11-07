81d8d7535897382c6b56a0f5424cd93a7d5a7c79
Anastasiiasyvak <asyvak@kse.org.ua>
2023-11-07 13:48:34 +0200

    coverage.py + .flake8 + .pylintrc

---
+++ coverage.py
@@ -0,0 +1,9 @@
+import multiprocessing
+from asyncio import run
+import venv
+
+[run]
+# source = /home/nastia/PycharmProjects/2_homework(fundamentals_of_software_engineering)
+omit = venv # Перелік файлів чи директорій, які потрібно пропустити
+concurrency = multiprocessing  # Використовуємо багатопроцесовий режим
+
